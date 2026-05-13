91c4e871de24cbdbd3491f67c8b25d4b064506ab
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-05-13 13:37:53 -0300

    Desenvolvendo o back-end: Rota: Detalhes do projeto

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -28,6 +28,7 @@ import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
 import { transferOrganization } from '@/http/routes/orgs/transfer-organization'
 import { createProject } from '@/http/routes/projects/create-project'
 import { deleteProject } from '@/http/routes/projects/delete-project'
+import { getProject } from '@/http/routes/projects/get-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -81,6 +82,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 app.register(createProject)
 app.register(deleteProject)
+app.register(getProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Server is running on http://localhost:3333')
