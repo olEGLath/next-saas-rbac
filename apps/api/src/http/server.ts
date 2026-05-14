ce7736fbab83fcc54e80b89a8029c8fd0e2582ea
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-05-14 16:57:52 -0300

    Desenvolvendo o back-end: Rota: Criação de convites

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -34,6 +34,7 @@ import { updateProject } from '@/http/routes/projects/update-project'
 import { getMembers } from '@/http/routes/members/get-members'
 import { updateMember } from '@/http/routes/members/update-member'
 import { removeMember } from '@/http/routes/members/remove-member'
+import { createInvite } from '@/http/routes/invites/create-invite'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -93,6 +94,7 @@ app.register(updateProject)
 app.register(getMembers)
 app.register(updateMember)
 app.register(removeMember)
+app.register(createInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Server is running on http://localhost:3333')
