f6e546b43b2bf5776408c84c646c53655f6bbf67
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-05-16 13:20:38 -0300

    Desenvolvendo o front-end: Setup do projeto Next.js

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,23 @@
+import { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foregrund: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
